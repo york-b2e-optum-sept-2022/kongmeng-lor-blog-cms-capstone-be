94eff2f0604da12ed1d684fbf05563111adf49ff
lorxx504-Kongmeng <lorxx504@umn.edu>
2022-12-05 11:03:14 -0600

    init

---
+++ src/test/java/net/yorksolutions/kongmenglorblogcmscapstonebe/KongmengLorBlogCmsCapstoneBeApplicationTests.java
@@ -0,0 +1,13 @@
+package net.yorksolutions.kongmenglorblogcmscapstonebe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KongmengLorBlogCmsCapstoneBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
